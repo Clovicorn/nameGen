0d3d29ebaa111c4a57d58ac51421407bc88e45c2
Clovicorn <109252472+Clovicorn@users.noreply.github.com>
2023-04-07 03:07:20 -0700

    added gitignore and tavern names gen.

--- nameGen.cpp
+++ nameGen.cpp
@@ -41,4 +41,32 @@ namespace NameGen
 
         return "Not implemented yet.";
     }
+
+    std::string NameGen::createTavernName(int timeMod)
+    {
+        std::string tavernName = "";
+
+        std::srand((int)std::time(nullptr) * timeMod);
+        float percent = (float)(std::rand() / RAND_MAX);
+
+        int index = 0;
+
+        if (percent < 0.1f)
+        {
+            std::srand((int)std::time(nullptr) * (timeMod + 1));
+            index = (int)(std::rand() % ARTICLE_TAVERN_NUMBER);
+            tavernName += _articleWordTavern[index];
+            tavernName += " ";
+        }
+        std::srand((int)std::time(nullptr) * (timeMod + 1));
+        index = (int)(std::rand() % FIRST_WORD_TAVERN_NUMBER);
+        tavernName += _firstWordTavern[index];
+        tavernName += " ";
+
+        std::srand((int)std::time(nullptr) * (timeMod + 1));
+        index = (int)(std::rand() % SECOND_WORD_TAVERN_NUMBER);
+        tavernName += _secondWordTavern[index];
+
+        return tavernName;
+    }
 }
--- nameGen.hpp
+++ nameGen.hpp
@@ -4,9 +4,7 @@
 #include <ctime>
 #include <cstdlib>
 
-#define PLANETFIRSTSYLLABLE 34
-#define PLANETMIDDLESYLLABLE 22
-#define PLANETLASTSYLLABLE 39
+#include "definitions.hpp"
 
 namespace NameGen
 {
@@ -16,11 +14,10 @@ namespace NameGen
         NameGen();
         std::string createPlanetName(int timeMod);
         std::string createTownName(int timeMod);
+        std::string createTavernName(int timeMod);
 
     private:
-        // ----- Planet variables here -----
-        //              |
-        //              v
+        // ----- Begin Planet variables -----
         bool _planetHasMiddleSyllable = false;
         std::string _firstSyllablePlanet[PLANETFIRSTSYLLABLE] = {"A", "Act", "Ae", "Al", "Apo", "Ar", "Au", "Ba", "Car", "Ce", "Cor", "Crom", "Er", "Hal", "Io", "La", "Lae", "Lao", "Lar", "Mae", "Men", "My", "Ne", "O", "Oe", "Pan", "Pha", "Phi", "Stri", "Sty", "Ta", "Ter", "Tro", "Xa"};
 
@@ -29,5 +26,12 @@ namespace NameGen
         std::string _lastSyllablePlanet[PLANETLASTSYLLABLE] = {"a", "dite", "don", "e", "es", "eon", "etes", "eus", "gone", "gos", "is", "ius", "jan", "k", "la", "laps", "lo", "ne", "nia", "nides", "nis", "noe", "on", "on", "os", "ous", "pes", "ra", "rana", "rion", "ryon", "s", "sia", "this", "thon", "thus", "us", "vus", "x"};
 
         // ------ End planet variables -------
+        // ------ Begin Tavern variables ------
+        std::string _articleWordTavern[ARTICLE_TAVERN_NUMBER] = {"Da", "The", "Thee", "Ye"};
+
+        std::string _firstWordTavern[FIRST_WORD_TAVERN_NUMBER] = {"Angry", "Bare", "Big", "Brass", "Chittering", "Dark", "Dirty", "Faded", "Fated", "Foaming", "Giant", "Grim", "Headless", "Lazy", "Light", "Long", "Prancing", "Single", "Small", "Stout", "Strong", "Tired", "Two-Headed"};
+
+        std::string _secondWordTavern[SECOND_WORD_TAVERN_NUMBER] = {"Boar", "Brew", "Bomb", "Bone", "Cat", "Chicken", "Cup", "Divide", "Dog", "End", "Fate", "Flame", "Flower", "Frog", "Goblet", "Hedge", "Horse", "House", "Nest", "Poison", "Pony", "Rabbit", "Rail", "Rock", "Side", "Snake", "Squirrel", "Table", "Wolf", "Wyrm"};
+        // ------ End Tavern variables ------
     };
 }
